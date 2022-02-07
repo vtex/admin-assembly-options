18d028573dce6a860adc0ab5102d6e4d3c40175d
GabrielBarros <gabriel.barros@vtex.com.br>
2022-02-07 16:07:08 -0300

    Admin UI adjustments and Intl messages added

--- react/AssemblyRegister.tsx
+++ react/AssemblyRegister.tsx
@@ -1,86 +1,40 @@
-import React, { useState } from 'react'
-import { Layout, PageHeader, PageBlock, Input, Checkbox } from 'vtex.styleguide'
+/* eslint-disable no-alert */
+import React from 'react'
+import { useIntl } from 'react-intl'
+import {
+  createSystem,
+  Page,
+  PageHeader,
+  PageTitle,
+  PageActions,
+  PageContent,
+  Button,
+} from '@vtex/admin-ui'
+
+import { messages } from './utils/messages'
+import RegisterForm from './components/RegisterForm'
 
 const AssemblyRegister = () => {
-  const [required, setRequired] = useState(false)
-  const [active, setActive] = useState(false)
+  const intl = useIntl()
+
+  const [ThemeProvider] = createSystem({
+    key: 'assembly-register-page',
+  })
 
   return (
-    <Layout
-      pageHeader={
-        <PageHeader
-          title="Assembly Register"
-          linkLabel="Assembly Options"
-          onLinkClick={() => {
-            alert('Voltar')
-          }}
-        />
-      }
-    >
-      <form>
-        <PageBlock>
-          <h3 className="f5 mb2">Main Config</h3>
-          <p className="f6 gray pb4 mb2">
-            Main information related to the entire Assembly Options
-          </p>
-          <div className="flex mv6 items-center mb2">
-            <div className="mr3 w-50">
-              <Input
-                placeholder="Name for the Assembly Options"
-                dataAttributes={{ 'hj-white-list': true, test: 'string' }}
-                label="Name"
-              />
-            </div>
-            <div className="w-50">
-              <Input
-                placeholder="ID"
-                dataAttributes={{ 'hj-white-list': true, test: 'string' }}
-                label="ID"
-                type="number"
-                disabled
-              />
-            </div>
-          </div>
-          <div className="flex mv6 items-center ">
-            <div className="mr3 w-50">
-              <Input
-                placeholder="Minimum Integer Number"
-                label="Minimum items applied "
-                type="number"
-                min="0"
-              />
-            </div>
-            <div className="w-50">
-              <Input
-                placeholder="Maximum Integer Number"
-                label="Maximum items applied "
-                type="number"
-                min="0"
-              />
-            </div>
-          </div>
-          <div className="flex mv6 items-center ">
-            <Checkbox
-              checked={active}
-              id="is-active"
-              label="Active"
-              name="default-checkbox-group"
-              onChange={() => setActive(!active)}
-              value="is-active"
-            />
-            <Checkbox
-              checked={required}
-              id="is-required"
-              className="mr5"
-              label="Required"
-              name="default-checkbox-group"
-              onChange={() => setRequired(!required)}
-              value="is-required"
-            />
-          </div>
-        </PageBlock>
-      </form>
-    </Layout>
+    <ThemeProvider>
+      <Page csx={{ background: '#F8F9FA' }}>
+        <PageHeader onPopNavigation={() => alert('onPopNavigation()')}>
+          <PageTitle>{intl.formatMessage(messages.pageTitle)}</PageTitle>
+          <PageActions>
+            <Button>Save</Button>
+          </PageActions>
+        </PageHeader>
+        <PageContent>
+          <RegisterForm />
+        </PageContent>
+      </Page>
+    </ThemeProvider>
   )
 }
 
---
+++ react/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable no-alert */
+import React from 'react'
+import { useIntl } from 'react-intl'
+import { Card, Heading, Paragraph, Flex, Box } from '@vtex/admin-ui'
+import { Formik } from 'formik'
+import { FormikInput, FormikCheckbox } from '@vtex/admin-formik'
+
+import { messages } from '../../utils/messages'
+
+const RegisterForm = () => {
+  const intl = useIntl()
+  const handleSubmit = () => {
+    alert('Values submitted: ')
+  }
+
+  return (
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={{
+        assembly: {
+          name: '',
+          id: '',
+          active: false,
+          required: false,
+        },
+      }}
+    >
+      <form>
+        <Card csx={{ marginTop: '15px' }}>
+          <Heading>{intl.formatMessage(messages.assemblyFormTitle)}</Heading>
+          <Paragraph>
+            {intl.formatMessage(messages.assemblyFormDescription)}
+          </Paragraph>
+          <Flex as="section" direction="row" csx={{ marginTop: '20px' }}>
+            <Box csx={{ width: '30%', paddingRight: 3 }}>
+              <FormikInput
+                name="assembly.name"
+                label={intl.formatMessage(messages.assemblyNameLabel)}
+              />
+            </Box>
+            <Box csx={{ width: '30%', paddingRight: 3 }}>
+              <FormikInput name="assembly.id" label="ID" disabled />
+            </Box>
+            <Box csx={{ width: '30%' }}>
+              <Flex direction="row" align="center" csx={{ height: '100%' }}>
+                <Box csx={{ paddingRight: 3 }}>
+                  <FormikCheckbox
+                    name="assembly.active"
+                    label={intl.formatMessage(messages.assemblyActive)}
+                  />
+                </Box>
+                <Box>
+                  <FormikCheckbox
+                    name="assembly.required"
+                    label={intl.formatMessage(messages.assemblyRequired)}
+                  />
+                </Box>
+              </Flex>
+            </Box>
+          </Flex>
+        </Card>
+      </form>
+    </Formik>
+  )
+}
+
+export default RegisterForm
