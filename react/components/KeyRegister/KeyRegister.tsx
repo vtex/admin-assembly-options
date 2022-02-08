52500c5820fdae08821748894f29b13680d0f79b
GabrielBarros <gabriel.barros@vtex.com.br>
2022-02-08 14:43:35 -0300

    Started working on the Assembly Key Config Element

--- react/components/KeyRegister/KeyRegister.tsx
+++ react/components/KeyRegister/KeyRegister.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Collapsible } from '@vtex/admin-ui'
+import {
+  Collapsible,
+  CollapsibleHeader,
+  CollapsibleContent,
+  useCollapsibleState,
+} from '@vtex/admin-ui'
+import { FormikInput } from '@vtex/admin-formik'
 
 const KeyRegister = () => {
+  const state = useCollapsibleState()
+
   return (
-    <Collapsible>
-      <h1>Oi</h1>
+    <Collapsible csx={{ width: '100%' }} state={state}>
+      <CollapsibleHeader label="Key Example" />
+      <CollapsibleContent>
+        <FormikInput name="" label="Key Name" />
+      </CollapsibleContent>
     </Collapsible>
   )
 }
