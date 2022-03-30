2641ff961b5333efe070d54cc1019019c4c009fb
GabrielBarros <gabriel.barros@vtex.com.br>
2022-03-30 16:37:20 -0300

    Desktop scroll being visible is now fixed

--- react/components/SKUGrid/SKUGrid.tsx
+++ react/components/SKUGrid/SKUGrid.tsx
@@ -124,7 +124,7 @@ const SKUGrid = (props: Props) => {
   })
 
   return (
-    <Flex csx={{ width: '100%', overflowX: 'scroll' }}>
+    <Flex csx={{ width: '100%', overflowX: ['scroll', 'unset'] }}>
       <DataGrid csx={{ marginTop: 5, width: '100%' }} state={state} />
     </Flex>
   )
